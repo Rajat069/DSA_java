5649769751db59c869e3e4c7cbab05431c9d56a4
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2022-04-04 15:26:07 +0530

    Time: 68 ms (46.29%), Space: 13.4 MB (38.86%) - LeetHub

---
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def minMovesToSeat(self, seats, students):
+            seats.sort()
+            students.sort()
+            ans=0
+            for i in range(len(seats)):
+                     ans+=(abs(seats[i]-students[i]))
+            return ans
+                
+        
