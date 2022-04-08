ccd22b00fe6fdf01540c4ba382cffbcc2f537afb
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2022-04-08 09:02:39 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (90.52%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,8 +1,10 @@
-class Solution(object):
-    def lastStoneWeight(self, stones):
-            stones.sort()
-            for i in range(len(stones)-2,-1,-1): 
-                stones.sort()
-                stones[i]=abs(stones[i+1]-stones[i])
-            return stones[0]    
-        
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        Arrays.sort(stones);
+        for(int i=stones.length-2;i>-1;i--){
+            Arrays.sort(stones);
+            stones[i]=Math.abs(stones[i+1]-stones[i]);
+        }
+        return stones[0];
+    }
+}
