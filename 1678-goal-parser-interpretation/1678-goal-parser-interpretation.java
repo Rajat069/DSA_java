37fb1e10aa63f13f3fbef0810e88ee810c29d1d4
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2022-04-09 13:50:40 +0530

    Time: 5 ms (10.76%), Space: 41 MB (82.98%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -2,21 +2,19 @@ class Solution {
     public String interpret(String com) {
         String ans ="";
         for(int i=0;i<com.length();i++){
-            if(com.charAt(i)=='G'){
-                ans+=com.charAt(i);
-            }
-            else if(com.charAt(i)=='('){
+                if(com.charAt(i)=='('){
                 if(com.charAt(i+1)==')'){
                     ans+='o';
                     i+=1;
                 }
-                else if((com.substring(i+1,i+4)).equals("al)")){
+                else {
                     ans+="al";
                     i+=3;
                     }
-            }
+                }
+            else  ans+=com.charAt(i);
           
-        }
+          }
           return ans;
     }
 }
