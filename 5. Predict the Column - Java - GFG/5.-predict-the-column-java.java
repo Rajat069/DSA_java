ad87f646db6cd35f22b448fd3bf8307843fa30f5
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2022-04-17 17:26:07 +0530

    Added solution - LeetHub

---
+++ 5. Predict the Column - Java - GFG/5.-predict-the-column-java.java
@@ -0,0 +1,70 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+ // } Driver Code Ends
+//User function Template for Java
+
+/*Class Geeks with its member function columnWithMaxZero()
+* a[][] = matrix input
+* n : number of rows and columns each
+*/
+class Geeks{
+    
+    static int columnWithMaxZero(int a[][],int n){
+       int maxx=0;
+       int col=0;
+       for(int i=0;i<a.length;i++){
+           int maxc=0;
+           for(int j=0;j<a[0].length;j++){
+               if(a[j][i]==0)maxc++;
+           }
+           if(maxc>maxx){
+              maxx=maxc;
+              col=i;
+           }
+       } 
+        return col;
+    }
+}
+
+// { Driver Code Starts.
+
+// Driver class
+class GFG {
+    
+    // Driver code
+	public static void main (String[] args) {
+	    // Taking input using scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking testcases
+		int testcase = sc.nextInt();
+		
+		while(testcase-- > 0){
+		    
+		    //taking size of 2d-array
+		    int n = sc.nextInt();
+
+            //creating a 2d-array
+		    int a[][] = new int[n][n];
+		    
+		    //adding elements to the array
+		    for(int i = 0;i<n;i++){
+		        for(int j = 0;j<n;j++){
+		            a[i][j] = sc.nextInt();
+		        }
+		    }
+		    
+		    //creating an object of Geeks clas
+		    Geeks obj = new Geeks();
+		    
+		    //calling the method columnWithMaxZero()
+		    //of class Geeks
+		    System.out.println(obj.columnWithMaxZero(a, n));
+		}
+	}
+}  // } Driver Code Ends
