3166844d9d69432d1fc9f01227768c7e0d849d1e
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2022-04-23 12:09:55 +0530

    Time: 7 ms (12.22%), Space: 44 MB (65.03%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -3,22 +3,18 @@ class Solution {
         int [] temp = new int[prices.length];
         Stack<Integer> st = new Stack<>();
         st.push(prices.length-1);
-        temp[prices.length-1]=-1;
+        temp[prices.length-1]=prices[prices.length-1];
         for(int i=prices.length-2;i>-1;i--){
             while(!st.isEmpty()&&prices[st.peek()]>prices[i]){
                 st.pop();
             }
             if(st.isEmpty()){
-                temp[i]=-1;
+                temp[i]=prices[i];
             }
-            else temp[i]=prices[st.peek()];
+            else temp[i]=prices[i]-prices[st.peek()];
             st.push(i);
         }
-        int[] ans = new int[prices.length];
-        for(int i=0;i<prices.length;i++){
-           if(temp[i]!=-1)ans[i]=prices[i]-temp[i];
-           else ans[i]=prices[i];
-        }
-        return ans;
+       
+        return temp;
     }
 }
