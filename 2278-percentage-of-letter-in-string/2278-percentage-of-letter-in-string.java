c132f8d27698435fb39f439b801592a57ff05f65
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2022-05-29 12:05:10 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (89.31%) - LeetHub

--- 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.java
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.java
@@ -1,33 +1,9 @@
 class Solution {
     public int percentageLetter(String s, char letter) {
-           String[] ar = s.split("");
-            int ans=0;
-            Arrays.sort(ar);
-            if(ar[0].charAt(0)<letter&&ar[ar.length/2].charAt(0)<=letter){
-                for(int i=ar.length-1;i>=0;i--){
-                    if(ar[i].equals(letter+"")){
-                        while(i<ar.length&&ar[i].equals(letter+"")) {
-                            ans++;
-                            i--;
-                        }
-                        break;
-                    }
-                }
-                float req = ans*100/ar.length;
-                return (int)req;
-            }
-            else{
-                for(int i=0;i<ar.length;i++){
-                    if(ar[i].equals(letter+"")){
-                        while(i<ar.length&&ar[i].equals(letter+"")) {
-                            ans++;
-                            i++;
-                        }
-                        break;
-                    }
-                }
-                float req = ans*100/ar.length;
-                return (int)req;
-            }
+        int ans =0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==letter)ans++;
         }
-}   
+        return (int)ans*100/s.length();
+    }
+}
