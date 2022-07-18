5038b3c166e309004c353152fd768e5c34485938
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2022-07-18 17:03:05 +0530

    Time: 24 ms (26.48%), Space: 74.5 MB (77.72%) - LeetHub

---
+++ 148-sort-list/148-sort-list.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+         if(head==null||head.next==null){
+             return head;
+         }
+         ListNode slow = head;
+         ListNode fast = head;
+         while(fast.next!=null&&fast.next.next!=null){
+             slow=slow.next;
+             fast=fast.next.next;
+         }
+        ListNode temp  = slow.next;
+        slow.next=null;
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(temp);
+        ListNode ans = mergeTwoLists(l1,l2);
+        return ans;
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode l1 = list1;
+        ListNode l2 = list2;
+        ListNode dummyhead  = new ListNode(0);
+        ListNode anshead = dummyhead;
+        while(l1!=null&&l2!=null){
+            if(l1.val>l2.val){
+                anshead.next = l2;
+                l2=l2.next;
+            }
+            else if(l1.val==l2.val){
+                 anshead.next = l1;
+                 l1=l1.next;
+                 anshead=anshead.next;
+                 anshead.next = l2;
+                 l2=l2.next;
+            }
+            else{
+                 anshead.next = l1;
+                 l1=l1.next;
+            }
+          anshead=anshead.next;  
+        }
+        while(l1!=null){
+            anshead.next=l1;
+            l1=l1.next;
+            anshead=anshead.next;
+        }
+        while(l2!=null){
+            anshead.next=l2;
+            l2=l2.next;
+            anshead=anshead.next;
+        }
+        return dummyhead.next;
+    }
+}
