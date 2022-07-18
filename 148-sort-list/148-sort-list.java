5c957e125d5e80f8cfdabaf2b2b3a168bd40ff9b
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2022-07-18 17:05:30 +0530

    Time: 19 ms (49.02%), Space: 74.8 MB (74.77%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -26,9 +26,7 @@ class Solution {
         ListNode ans = mergeTwoLists(l1,l2);
         return ans;
     }
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode l1 = list1;
-        ListNode l2 = list2;
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummyhead  = new ListNode(0);
         ListNode anshead = dummyhead;
         while(l1!=null&&l2!=null){
@@ -36,29 +34,13 @@ class Solution {
                 anshead.next = l2;
                 l2=l2.next;
             }
-            else if(l1.val==l2.val){
-                 anshead.next = l1;
-                 l1=l1.next;
-                 anshead=anshead.next;
-                 anshead.next = l2;
-                 l2=l2.next;
-            }
             else{
                  anshead.next = l1;
                  l1=l1.next;
             }
           anshead=anshead.next;  
         }
-        while(l1!=null){
-            anshead.next=l1;
-            l1=l1.next;
-            anshead=anshead.next;
-        }
-        while(l2!=null){
-            anshead.next=l2;
-            l2=l2.next;
-            anshead=anshead.next;
-        }
+        anshead.next = l1!=null?l1:l2;
         return dummyhead.next;
     }
 }
