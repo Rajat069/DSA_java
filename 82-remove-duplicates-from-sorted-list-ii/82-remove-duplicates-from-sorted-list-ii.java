381c1098ff6f57c978ad589ede4462faf44aabca
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2022-08-01 17:06:36 +0530

    Time: 1 ms (84.51%), Space: 43.3 MB (75.05%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.java
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.java
@@ -10,8 +10,9 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummynode = new ListNode(-1);
+        ListNode ansnode = dummynode;
         ListNode temp = head;
-        ArrayList<Integer> ar = new ArrayList<>();
         while(temp!=null){
            int count = 0; 
            while(temp.next!=null&&temp.val==temp.next.val){
@@ -19,18 +20,12 @@ class Solution {
                count++;
            }
            if(count==0){
-               ar.add(temp.val);
+               ansnode.next=temp;
+               ansnode=ansnode.next;
            } 
            temp=temp.next;
         }
-        Collections.sort(ar);
-        ListNode dummynode = new ListNode(-1);
-        ListNode ansnode = dummynode;
-        int i=0;
-        while(i<ar.size()){
-            ansnode.next= new ListNode(ar.get(i++));
-            ansnode=ansnode.next;
-        }
+        ansnode.next=null;
         return dummynode.next;
     }
 }
