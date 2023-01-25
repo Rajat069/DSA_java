6d5a4296c7f73e7d10ff48aab8148a0be78f7b72
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-01-25 22:23:26 +0530

    Time: 8 ms (46.61%), Space: 42.9 MB (25.45%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -8,16 +8,15 @@ class Solution {
                 continue;
             }
             int j=i+1;
-            boolean f=false;
             while(j<s.length()&&j<=maxidx){
-                if(s.lastIndexOf(s.charAt(j))>maxidx){
-                    maxidx=s.lastIndexOf(s.charAt(j));
+                int idx=s.lastIndexOf(s.charAt(j));
+                if(idx>maxidx){
+                    maxidx=idx;
                 }
                 j++;
-                f=true;
             }
             ans_list.add(j-i);
-            if(f)i=j-1;
+            if(i!=j)i=j-1;
         }
         return ans_list;
     }
