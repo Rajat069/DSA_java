bde4cc63af76129a8cee437f09699bc66b7fa229
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-01-26 22:04:49 +0530

    Time: 10 ms (42.72%), Space: 19.5 MB (16.50%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.php
+++ 0290-word-pattern/0290-word-pattern.php
@@ -8,16 +8,14 @@ class Solution {
     function wordPattern($pattern, $s) {
         $ar=explode(" ",$s);
         $len=count($ar);
-        $ar_check=array();
+        $map=array();
         if(strlen($pattern)!=$len)return false;
         for($i=0;$i<count($ar);$i++){
-           if(array_key_exists($pattern[$i],$ar_check)&&$ar_check[$pattern[$i]]!=$ar[$i])return false;
-            else $ar_check[$pattern[$i]]=$ar[$i];
-        }
-         $counts = array_count_values($ar_check);
-        foreach ($counts as $count) {
-            if ($count > 1) {
-                return false;
+            if(array_key_exists($pattern[$i],$map)){
+                if($map[$pattern[$i]]!=$ar[$i]) return false;
+            }else{
+                if(in_array($ar[$i],$map)) return false;
+                $map[$pattern[$i]] = $ar[$i];
             }
         }
         return true;
