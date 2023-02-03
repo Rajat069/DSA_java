7ae0ff2fbef3f2cf3b4478fb8a4da6e701ffb220
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-02-03 11:13:12 +0530

    Time: 40 ms (55.67%), Space: 49.2 MB (56.72%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,9 +1,9 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        HashSet<String>dupli_checker=new HashSet<>();
         List<List<Integer>> ans_list=new ArrayList<>();
         for(int i=0;i<nums.length-2;i++){
+            if(i>0&&nums[i]==nums[i-1])continue;
             int temp=nums[i];
             int f=i+1;
             int l=nums.length-1;
@@ -11,13 +11,11 @@ class Solution {
                 int val=temp+nums[f]+nums[l];
                 if(val==0){
                    List<Integer>ls=Arrays.asList(temp,nums[f],nums[l]);
-                   String dup=ls.toString();
-                   if(!dupli_checker.contains(dup)){
-                        ans_list.add(ls); 
-                   }
-                   dupli_checker.add(dup); 
+                   ans_list.add(ls);
                    f++;
                    l--;
+                   while(f<l&&nums[f]==nums[f-1])f++;
+                   while(f<l&&nums[l]==nums[l+1])l--; 
                 }
                 else if(val>0){
                     l--;
