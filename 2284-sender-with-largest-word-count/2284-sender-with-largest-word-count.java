b3bd652f2e4952866b3b023695cd218a446d0887
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-02-13 16:09:35 +0530

    Time: 57 ms (71.74%), Space: 50.1 MB (54.35%) - LeetHub

--- 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.java
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.java
@@ -2,19 +2,17 @@ class Solution {
     public String largestWordCount(String[] messages, String[] senders) {
         HashMap<String,Integer> hm = new HashMap<>();
         int idx=0;
+        String big_name= "";
+        int cur_big=0;
         for(String name:senders){
             String[] words = messages[idx++].split(" ");
             hm.put(name,hm.getOrDefault(name,0)+words.length);
-        }
-        String big_name= "";
-        int cur_big=0;
-        for(String name:hm.keySet()){
             int count=hm.get(name);
-            if(cur_big<count){
-                cur_big=count;
+            if(count==cur_big&&big_name.compareTo(name)<0){
                 big_name=name;
             }
-            else if(count==cur_big&&name.compareTo(big_name)>0){
+            else if(cur_big<count){
+                cur_big=count;
                 big_name=name;
             }
         }
