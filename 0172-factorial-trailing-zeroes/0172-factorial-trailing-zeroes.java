6e101215bbd1a2b4e4b9192576beebb119f68dbf
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-03-03 17:36:57 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (76.23%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int trail=0;
+        for(int i=5;i<=n;i*=5){
+            trail+=(n/i);
+        }
+        return trail;
+    }
+}
