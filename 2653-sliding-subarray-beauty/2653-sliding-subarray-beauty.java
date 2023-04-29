c24d97203c475469b95b140d19644609ef097b0c
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-04-29 20:04:27 +0530

    Time: 35 ms (93.18%), Space: 59.4 MB (66.31%) - LeetHub

--- 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.java
+++ 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.java
@@ -1,46 +1,34 @@
 class Solution {
     public int[] getSubarrayBeauty(int[] nums, int k, int x) {
-        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
-        int i = 0;
-        Integer val = null; // use Integer instead of int to allow for null values
-        for (; i < k; i++) {
-            treeMap.put(nums[i], treeMap.getOrDefault(nums[i], 0) + 1);
-        }
-        int loc = 1,f=0;
-        for (int q : treeMap.keySet()) {
-            int freq = treeMap.get(q);
-            if(q<0)f+=freq;
-            else if(q>0)break;
-            if(f>=x){
-                val=q;
-                break;
-            }
-        }
-        int[] sol = new int[nums.length - k + 1];
-        sol[0] = val != null ? val : 0; // if no negative number was found, set val to 0
-        int idx = 1;
-        for (; i < nums.length; i++) {
-            int prev = nums[idx - 1];
-            if (treeMap.containsKey(prev)) {
-                treeMap.put(prev, treeMap.get(prev) - 1);
-                if (treeMap.get(prev) == 0) {
-                    treeMap.remove(prev);
-                }
-            }
-            treeMap.put(nums[i], treeMap.getOrDefault(nums[i], 0) + 1);
-            int l = 1, v = 0;
-            f=0;
-            for (int q : treeMap.keySet()) {
-                int freq = treeMap.get(q);
-                if(q<0)f+=freq;
-                else if(q>0)break;
-                 if(f>=x){
-                   v=q;
-                   break;
-               }
-            }
-            sol[idx++] = v; // if no negative number was found, set v to 0
-        }
+          int[] count = new int[50];
+          int[] sol = new int[nums.length-k+1];
+          int i=0;
+          for(;i<k;i++){
+              if(nums[i]<0)count[nums[i]+50]++;
+          }
+          int l=0,val=0;
+          for(int q=0;q<50;q++){
+                  l+=count[q];
+                  if(l>=x){
+                      val=q-50;
+                      break;
+                  }
+          }
+          sol[0]=val;
+          int idx=1;
+          for(;i<nums.length;i++){
+              if(nums[idx-1]<0)count[nums[idx-1]+50]--;
+              if(nums[i]<0)count[nums[i]+50]++;
+              int loc=0,v=0;
+              for(int q=0;q<50;q++){
+                  loc+=count[q];
+                  if(loc>=x){
+                      v=q-50;
+                      break;
+                  }
+              }
+              sol[idx++]=v;
+          }
         return sol;
     }
 }
