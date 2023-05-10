c9d9b248f2f45fc5eb6e90a6075b1b22c9189a51
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-05-10 18:24:33 +0530

    Time: 1 ms (87.02%), Space: 43 MB (29.82%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,32 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> sol = new ArrayList<>();
-        if (root == null) {
-            return sol;
-        }
-        
-        Queue<TreeNode> orderQ = new ArrayDeque<>();
+        if(root==null)return sol;
+        Queue<TreeNode> orderQ = new ArrayDeque();
         orderQ.add(root);
-        
-        while (!orderQ.isEmpty()) {
-            int size = orderQ.size();
-            List<Integer> temp = new ArrayList<>();
-            
-            for (int i = 0; i < size; i++) {
-                TreeNode node = orderQ.poll();
-                temp.add(node.val);
-                
-                if (node.left != null) {
-                    orderQ.add(node.left);
+        while(orderQ.size()>0){
+            ArrayList<Integer>temp = new ArrayList<>();
+            int size=orderQ.size();
+            int q=0;
+            while(q<size){
+                TreeNode t = orderQ.poll();
+                temp.add(t.val);
+                if(t.left!=null){
+                    orderQ.add(t.left);
                 }
-                if (node.right != null) {
-                    orderQ.add(node.right);
+                if(t.right!=null){
+                    orderQ.add(t.right);
                 }
+               q++;
             }
-            
             sol.add(temp);
         }
-        
         return sol;
     }
 }
