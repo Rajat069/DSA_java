953263663e0daa6b45fb7c0687ad6f32503c3f4d
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-05-17 14:47:07 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (7.58%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int pvt=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<nums[i-1]){
+               pvt=i;
+                break;
+            }
+        }
+        boolean p1=binSearch(nums,0,pvt,target);
+        return !p1?binSearch(nums,pvt,nums.length-1,target):p1;
+    }
+    public boolean binSearch(int[] nums,int f,int l,int tar){
+        while(f<=l){
+            int mid = f+(l-f)/2;
+            if(nums[mid]==tar)return true;
+            else if(nums[mid]>tar)l=mid-1;
+            else f=mid+1;
+        }
+        return false;
+    }
+}
