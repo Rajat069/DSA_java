8a27eab74b10d1517f57b769c111127d84cc7239
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-06-03 16:35:00 +0530

    Time: 3 ms (95.81%), Space: 47.6 MB (93.78%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int no=0,n=grid.length,m=grid[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    visit(grid,i,j);
+                    no++;
+                }
+            }
+        }
+        return no;
+    }
+     public void visit(char[][]grid,int i,int j){
+        if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]=='0')return; //out of bounds or visted cells conditions
+        grid[i][j]='0';//setting visted cell to 0 to escape from infinte loop!
+        //four recursive call for up left right down neighbours
+        visit(grid,i+1,j);
+        visit(grid,i,j+1);
+        visit(grid,i,j-1);
+        visit(grid,i-1,j);
+        return;
+    }
+}
