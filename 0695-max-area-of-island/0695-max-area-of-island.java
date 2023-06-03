89cbf263c6dd402e9957c1cdee1761dc831b08ad
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-06-03 16:27:26 +0530

    Time: 2 ms (100.00%), Space: 43 MB (40.07%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         int n=grid.length,m=grid[0].length,max=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){  //traversing each valid island piece
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
                     int count=count_area(grid,i,j);   
@@ -12,10 +12,10 @@ class Solution {
         return max;
     }
     public int count_area(int[][]grid,int i,int j){
-        if(i<0||j<0||i>=grid.length||j>=grid[0].length)return 0;
-        if(grid[i][j]==0)return 0;
-        grid[i][j]=0;
+        if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]==0)return 0; //out of bounds or visted cells conditions
+        grid[i][j]=0;//setting visted cell to 0 to escape from infinte loop!
         int area=1;
+        //four recursive call for up left right down neighbours
         area+=count_area(grid,i+1,j);
         area+=count_area(grid,i,j+1);
         area+=count_area(grid,i,j-1);
