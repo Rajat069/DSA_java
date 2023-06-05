90368b846ea8681df1eefd6a8fc0597ad8bb600a
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-06-05 18:21:24 +0530

    Time: 73 ms (50.00%), Space: 54.7 MB (33.33%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<String> removeSubfolders(String[] folder) {
         List<String>sol = new ArrayList<>();
-        HashSet<String> hs = new HashSet<>();
-        Arrays.sort(folder,(a,b)->(a.length()-b.length()));
+        HashSet<String> hs = new HashSet<>();  //for contains Method
+        Arrays.sort(folder,(a,b)->(a.length()-b.length())); //sorting according to length
         for(String s:folder){
-            String subs[] = s.split("/");
+            String subs[] = s.split("/"); //spliting into subfolders
             StringBuilder fld=new StringBuilder();
             boolean add=true;
             for(String fl:subs){
-                if(fl.length()!=0)fld.append("/"+fl);
-                if(hs.contains(fld.toString())){
-                    add=false;
+                if(fl.length()!=0)fld.append("/"+fl); //if not empty(empty string is formed due to "/a" splitting into "" and a) we append it to the sb to check for parent folder
+                if(hs.contains(fld.toString())){ //if it already exists in the system we continue ahead and dont add it to the arraylist by doing the add to false and breaking out of the loop
+                    add=false; 
                     break;
                 }
             }
-            if(add){
+            if(add){ // we only add both incase it dont exists
               sol.add(s);
               hs.add(s);
             }
