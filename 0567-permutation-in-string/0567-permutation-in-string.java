d83d23b483395048431b575a0c361920d8728e9a
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-06-05 22:50:31 +0530

    Time: 8 ms (62.23%), Space: 41.7 MB (98.28%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,26 +1,23 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) {
-            return false;
+        if(s1.length()>s2.length())return false;
+        int[] freq1 = new int[26],freq2 = new int[26];
+        for(int i=0;i<s1.length();i++){
+            freq1[s1.charAt(i)-'a']++;
+            freq2[s2.charAt(i)-'a']++;
         }
-
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
-
-        for (int i = 0; i < s1.length(); i++) {
-            freq1[s1.charAt(i) - 'a']++;
-            freq2[s2.charAt(i) - 'a']++;
+        for(int i=s1.length(),idx=0;i<s2.length();i++,idx++){
+            boolean flag =true;
+            for(int q=0;q<26;q++){
+              if(freq1[q]!=freq2[q]){
+                  flag=false;
+                  break;
+              }
+             }
+            if(flag)return true;
+            freq2[s2.charAt(idx)-'a']--;
+            freq2[s2.charAt(i)-'a']++;
         }
-
-        for (int i = s1.length(); i < s2.length(); i++) {
-            if (Arrays.equals(freq1, freq2)) {
-                return true;
-            }
-
-            freq2[s2.charAt(i - s1.length()) - 'a']--;
-            freq2[s2.charAt(i) - 'a']++;
-        }
-
-        return Arrays.equals(freq1, freq2);
+        return Arrays.equals(freq1,freq2);
     }
 }
