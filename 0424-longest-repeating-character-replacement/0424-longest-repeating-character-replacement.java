26b9c56ed88c7cb45dd07b5144e4edc1622f649b
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-07-09 12:44:13 +0530

    Time: 9 ms (67.93%), Space: 41.6 MB (73.86%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -4,12 +4,12 @@ class Solution {
         int maxlen=0,mfreq=0;
         int[] freq = new int[26];
         for(int r=0;r<s.length();r++){
-            freq[s.charAt(r)-'A']++;
-            mfreq=Math.max(freq[s.charAt(r)-'A'],mfreq);
-            if(r-l-mfreq+1>k){
-                freq[s.charAt(l++)-'A']--;
+            freq[s.charAt(r)-'A']++;  //keep count of each char
+            mfreq=Math.max(freq[s.charAt(r)-'A'],mfreq); //calculating maxfreq character
+            if(r-l-mfreq+1>k){ //if length of string-most common character Eg. AAFG 4-2 = 2 is need to change 
+                freq[s.charAt(l++)-'A']--; //incrementing l and decrementing freq of l moving widow AABB to ABBC sliding over it 
             }
-            maxlen=Math.max(r-l+1,maxlen);
+            maxlen=Math.max(r-l+1,maxlen); //updating length
         }
         return maxlen;
     }
