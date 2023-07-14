e232ebd16ca77d429ad27747f7d4db2958564e53
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-07-14 11:54:55 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.java
+++ Subset Sum Problem - GFG/subset-sum-problem.java
@@ -38,22 +38,22 @@ class Solution{
 
     static Boolean isSubsetSum(int N, int arr[], int sum){
         boolean[][] dp = new boolean[N+1][sum+1];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                if(i==0&&j==0){
+        for(int i=0;i<dp.length;i++){ //i rep elements of given array
+            for(int j=0;j<dp[0].length;j++){ // j rep runs 0 till taget
+                if(i==0&&j==0){ //first cell here can make 0 run as empty set can form 0 run
                     dp[i][j]=true;
                 }
-                else if(i==0){
+                else if(i==0){ // first row will be false as empty set cannot be used to make any runs except 0
                     dp[i][j]=false;
                 }
-                else if(j==0){
+                else if(j==0){  //first collumn will be true as 0 run can be formed by not participating in subset
                     dp[i][j]=true;
                 }
                 else{
-                    if(dp[i-1][j]==true)dp[i][j]=dp[i-1][j];
-                    else{
-                        int rem = arr[i-1];
-                        if(j>=rem && dp[i-1][j-rem]==true){
+                    if(dp[i-1][j]==true)dp[i][j]=dp[i-1][j]; // if team can form run there is no need to include element in subset
+                    else{  
+                        int rem = arr[i-1];  // if it can then we will have to check if selected element can score greator then req run
+                        if(j>=rem && dp[i-1][j-rem]==true){ // then we will check if remaing runs can be formed by team or not 7-4= 3?? possible
                             dp[i][j]=true;
                         }
                     }
