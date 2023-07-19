8a5b1489716d81332084a61cd1bf1c862d4ada88
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-07-19 10:49:46 +0530

    Time: 12 ms (18.23%), Space: 45.7 MB (52.06%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -19,15 +19,6 @@ class Solution {
               p++;
           }
           ar.add(new int[]{start,end});
-          int[][] sol = new int[ar.size()][2];
-          p=0;
-          while(p<ar.size()){
-              int[] temp = new int[2];
-              temp[0]=ar.get(p)[0];
-              temp[1]=ar.get(p)[1];
-              sol[p]=temp;
-              p++;
-          }
-        return sol;
+        return ar.toArray(new int[ar.size()][2]);
     }
 }
