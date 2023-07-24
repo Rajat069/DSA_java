b697a822f6170752e36808eeddc8305ae15b6b49
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-07-24 19:13:38 +0530

    Time: 1 ms (16.69%), Space: 40.8 MB (52.92%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,19 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-          double sol=1;
-          long N=n; 
-          if(N<0)N*=-1;
-          while(N!=0){
-              if(N%2==0){
-                  x=x*x; //if pow is even do square of number and reduces power by 1/2 like 2^6 = 4^3 
-                  N/=2;
-              }
-              else{
-                  sol*=x; //if even then multply sol by number once and reduce pow like 4^3 to 1*4 = 4 and 4^2 now sol=16 pow=1 now 16*16 = 64 as pow is 1 and it needs to be 0 to be out of loop
-                  N--;
-              }
-          }
-        if(n<0)return 1.0/sol;
-        return sol;
+       return helper(x,Long.valueOf(n));
+    }
+    public double helper(double num,long pow){
+        if (num < -10000 || num > 10000) {
+            return 0;
+        }
+        if(pow==0)return 1;
+        if(pow<0){
+            pow*=-1;
+            num=1/num;
+        }
+        if(pow%2==0){
+            return helper(num*num,pow/2);
+        }
+        else return num*helper(num,pow-1);
     }
 }
