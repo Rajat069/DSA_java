599efa6f23d4fd66866927ca54300a7730bddd97
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-07-24 18:54:24 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (10.17%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -5,11 +5,11 @@ class Solution {
           if(N<0)N*=-1;
           while(N!=0){
               if(N%2==0){
-                  x=x*x;
+                  x=x*x; //if pow is even do square of number and reduces power by 1/2 like 2^6 = 4^3 
                   N/=2;
               }
               else{
-                  sol*=x;
+                  sol*=x; //if even then multply sol by number once and reduce pow like 4^3 to 1*4 = 4 and 4^2 now sol=16 pow=1 now 16*16 = 64 as pow is 1 and it needs to be 0 to be out of loop
                   N--;
               }
           }
