827aa6c6ac9e7558422e5e1706f1971a05c64fef
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-07-28 09:49:20 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (18.47%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,13 +1,20 @@
 class Solution {
     public boolean PredictTheWinner(int[] nums) {
-        return Pwinner(0,nums.length-1,nums)>=0;
-    }
-    public int Pwinner(int l,int r,int[]nums){
-        if(l==r){
-            return nums[l];
+        int[][]dp = new int[nums.length][nums.length];
+        for(int i=0;i<dp.length;i++){
+            Arrays.fill(dp[i],-1);
         }
-        int lscore=nums[l]-Pwinner(l+1,r,nums);
-        int rscore=nums[r]-Pwinner(l,r-1,nums);
-        return Math.max(lscore,rscore);
+        return PWinner(nums,dp,0,nums.length-1)>=0; 
+    }
+    public int PWinner(int[]nums,int[][]dp,int l,int r){
+        if(dp[l][r]!=-1){
+            return dp[l][r];
+        }
+        if(l==r)return nums[l];
+        int lscore = nums[l]-PWinner(nums,dp,l+1,r);
+        int rscore = nums[r]-PWinner(nums,dp,l,r-1);
+        int maxS=Math.max(lscore,rscore);
+        dp[l][r]=maxS;
+        return dp[l][r];
     }
 }
