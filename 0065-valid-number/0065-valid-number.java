3ac1d34c6bf87107724c7d7c8b78428ad3a4ccb2
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-07-30 20:46:18 +0530

    Time: 1 ms (100.00%), Space: 41.3 MB (97.54%) - LeetHub

--- 0065-valid-number/0065-valid-number.java
+++ 0065-valid-number/0065-valid-number.java
@@ -1,27 +1,34 @@
 class Solution {
     public boolean isNumber(String s) {
-        int signC=0,dot=0,e=0,num=0;
-        int eLoc=Integer.MAX_VALUE;
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(c=='+'||c=='-'){
-               signC++;
-               if(signC>1&&Character.toUpperCase(s.charAt(i-1))!='E'||i!=0&&Character.toUpperCase(s.charAt(i-1))!='E'||i==s.length()-1)return false; 
-            }
-            else if(c=='e'||c=='E'){
-                if(i==s.length()-1||i<s.length()-2&&!Character.isDigit(s.charAt(i+2)))return false;
+        int signC = 0, dot = 0, e = 0, num = 0;
+        int eLoc = Integer.MAX_VALUE;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '+' || c == '-') {
+                if (i != 0 && Character.toUpperCase(s.charAt(i - 1)) != 'E')
+                    return false;
+                signC++;
+                if (signC > 2 || (signC == 2 && eLoc != i - 1) || i == s.length() - 1)
+                    return false;
+            } else if (c == 'e' || c == 'E') {
+                if (i == s.length() - 1 || i == 0 || eLoc < i || num == 0)
+                    return false;
                 e++;
-                eLoc=i;
-                if(e>1||i==s.length()-1||i==0||num==0||i!=0&&s.charAt(i-1)=='-')return false;
-            }
-            else if(c=='.'){
+                eLoc = i;
+                if (e > 1 || s.charAt(i - 1) == '-')
+                    return false;
+            } else if (c == '.') {
                 dot++;
-                if(dot>1||eLoc<i||s.length()==1)return false;
+                if (dot > 1 || eLoc < i || (i + 1 == s.length() && num == 0) || (i + 1 < s.length() && !Character.isDigit(s.charAt(i + 1)) && s.charAt(i + 1) != 'e' && s.charAt(i + 1) != 'E'))
+                    return false;
+            } else if (!Character.isDigit(c)) {
+                return false;
+            } else {
+                num++;
             }
-            else if(Character.isAlphabetic(c)&&Character.toUpperCase(c)!='E')return false;
-            else if(Character.isDigit(c))num++;
         }
-        if(signC>0&&e==0&&s.charAt(0)!='-'&&s.charAt(0)!='+'||signC>0&&num==0)return false; 
+        if (e > 0 && num == 0)
+            return false;
         return true;
     }
 }
