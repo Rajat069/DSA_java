85c0b4385ed9dea63ecd44d7547cd5a45c6ba26b
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-07-31 21:07:30 +0530

    Time: 79 ms (26.38%), Space: 44.8 MB (16.08%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,30 +1,40 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
-        List<List<Integer>> sol = new  ArrayList<>();
+        HashSet<List<Integer>> sol = new  HashSet<>();
+        HashMap<Long,ArrayList<Integer>> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(Long.valueOf(nums[i]), map.getOrDefault(Long.valueOf(nums[i]), new ArrayList<>()));
+            map.get(Long.valueOf(nums[i])).add(i);
+        }       
         for(int i=0;i<nums.length;i++){
            if(i>0&&nums[i]==nums[i-1])continue;
             for(int j=i+1;j<nums.length;j++){
                if(j>i+1&&nums[j]==nums[j-1])continue;
                  for(int q=j+1;q<nums.length;q++){
                    if(q>j+1&&nums[q]==nums[q-1])continue;
-                    for(int t=q+1;t<nums.length;t++){
-                        if(t>q+1&&nums[t]==nums[t-1])continue;
-                        long sum = nums[i]+nums[j];
-                        sum+=nums[q]+nums[t];
-                        if(sum==target){
-                            List<Integer> ar = new ArrayList<>();
-                            ar.add(nums[i]);
-                            ar.add(nums[j]);
-                            ar.add(nums[q]);
-                            ar.add(nums[t]);
-                            sol.add(ar);
-                        }
-                    } 
+                      long sum =nums[i]+nums[j];
+                      sum+=nums[q];
+                      long val = target-sum; 
+                      if(map.containsKey(val)){
+                            ArrayList<Integer> index = map.get(val);
+                            for(int idx:index){
+                                if(idx>q){
+                                   List<Integer> ar = new ArrayList<>();
+                                   ar.add(nums[i]);
+                                   ar.add(nums[j]);
+                                   ar.add(nums[q]);
+                                   ar.add(nums[idx]);
+                                   sol.add(ar);
+                                }
+                            }
+                        } 
                  }
             }
             
         }
-        return sol;
+        List<List<Integer>> soll = new ArrayList<>();
+        soll.addAll(sol);
+        return soll;
     }
 }
