37cb7c5ef7c75f868012d34e029c12ef54f11650
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-08-02 15:14:55 +0530

    Time: 0 ms (100.00%), Space: 43.9 MB (6.64%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
@@ -1,28 +1,28 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public TreeNode sortedArrayToBST(int[] nums) {
-        return helper(nums,0,nums.length-1);
+        // Call the helper function to construct a balanced BST using the sorted array.
+        // The initial range for the array is from index 0 to nums.length-1.
+        return helper(nums, 0, nums.length - 1);
     }
-    public TreeNode helper(int[] nums,int low,int high){
-        if(low>high)return null;
-        int mid = low+(high-low)/2;
+    
+    public TreeNode helper(int[] nums, int low, int high) {
+        // Base case: If the low index is greater than the high index, the range is invalid.
+        if (low > high) {
+            return null;
+        }
+        
+        // Calculate the middle index of the current range.
+        int mid = low + (high - low) / 2;
+        
+        // Create a new TreeNode with the value at the middle index of the nums array.
         TreeNode node = new TreeNode(nums[mid]);
-        node.left=helper(nums,low,mid-1);
-        node.right=helper(nums,mid+1,high);
+        
+        // Recursively construct the left and right subtrees using the left and right halves
+        // of the current range in the nums array, respectively.
+        node.left = helper(nums, low, mid - 1);
+        node.right = helper(nums, mid + 1, high);
+        
+        // Return the newly constructed TreeNode.
         return node;
     }
 }
