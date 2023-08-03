69c76ad73f57bf74db0b062353e9a608c8f00fc1
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-08-03 16:28:38 +0530

    Time: 19 ms (51.12%), Space: 44.4 MB (86.19%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -12,17 +12,17 @@ public class Codec {
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
         if(root==null)return "";
-        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>(); //using ll lets us use null into our BFS
         StringBuilder encoded = new StringBuilder();
         queue.add(root);
         while(!queue.isEmpty()){
                 TreeNode temp = queue.poll();
                 if(temp==null){
                     encoded.append("N,");
-                    continue;
+                    continue; // if null skip rest
                 }
                 encoded.append(temp.val+",");
-                queue.add(temp.left);
+                queue.add(temp.left); //null will be added to queue if it is null
                 queue.add(temp.right);
         }
         return encoded.toString();
@@ -31,13 +31,13 @@ public class Codec {
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
         if(data=="")return null;
-        String[] nodes = data.split(",");
+        String[] nodes = data.split(","); // "," as a delimitter
         Queue<TreeNode> queue = new LinkedList<>();
-        TreeNode root=new TreeNode(Integer.valueOf(nodes[0]));
+        TreeNode root=new TreeNode(Integer.valueOf(nodes[0])); 
         queue.add(root);
-        for(int i=1;i<nodes.length;i++){
+        for(int i=1;i<nodes.length;i++){ //checking all strings Node,left,right,Node....... 
                 TreeNode pNode = queue.poll();
-                if(!nodes[i].equals("N")){
+                if(!nodes[i].equals("N")){  
                     TreeNode left = new TreeNode(Integer.valueOf(nodes[i]));
                     pNode.left = left;
                     queue.add(left);
