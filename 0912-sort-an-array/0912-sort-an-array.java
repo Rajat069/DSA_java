c9a9eb20363af80dcbdee64cd631ac6fc3c1bf1d
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-08-05 16:45:08 +0530

    Time: 27 ms (81.77%), Space: 54.9 MB (82.61%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,13 +1,45 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        PriorityQueue<Integer>pq = new PriorityQueue<>();
-        for(int i:nums){
-            pq.offer(i);
+        mergeSort(nums,0,nums.length-1);
+        return nums;
+    }
+    void mergeSort(int[]nums,int low,int high){
+        if(low<high){
+            int mid = (high - low) / 2 + low;
+            mergeSort(nums,low,mid);
+            mergeSort(nums,mid+1,high);
+            merge(nums,low,mid,high);
         }
-        int[]sol = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            sol[i]=pq.poll();
+    }
+    void merge(int arr[], int l, int m, int r)
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+        for (int i = 0; i < n1; i++){
+            L[i] = arr[l + i];   
         }
-        return sol;
+        for (int j = 0; j < n2; j++){
+            R[j] = arr[m + 1 + j];   
+        }
+        int i = 0, j = 0,k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+         while (i < n1) {
+            arr[k++] = L[i++];
+        }
+         while (j < n2) {
+            arr[k++] = R[j++];
+        }
     }
 }
