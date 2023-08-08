b4920f7047f09cedfa275c907c72a82e04b8521a
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-08-08 15:51:40 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (28.89%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,19 +1,17 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int pvt =0;
-        int max=Integer.MIN_VALUE;
-        int idx=0;
-        for(int i:nums){
-            if(i>max){
-                pvt=idx;
-                max=i;
-            }
-            idx++;
-        }
-        System.out.println(pvt);
-        int val=bsearch(nums,0,pvt,target);
-        return val==-1?bsearch(nums,pvt+1,nums.length-1,target):val;
-        
+       int pvt=-1;
+       for(int i=1;i<nums.length;i++){
+           if(nums[i]<nums[i-1]){
+               pvt=i-1;
+               break;
+           }
+       } 
+       if(pvt==-1){
+           return bsearch(nums,0,nums.length-1,target);
+       } 
+       int bsearch1 = bsearch(nums,0,pvt,target); 
+       return bsearch1 == -1?bsearch(nums,pvt+1,nums.length-1,target):bsearch1;
     }
     public int bsearch(int[]ar,int f,int l,int tar){
         while(f<=l){
