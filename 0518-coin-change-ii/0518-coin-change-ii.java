f99c352f49a0d2b11d879fc87cd9f7491169acfb
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-08-11 16:46:28 +0530

    Time: 2 ms (100.00%), Space: 39.6 MB (98.05%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,14 +1,11 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int[]dp = new int[amount+1];
-        for(int i=0;i<coins.length;i++){
-            for(int j=0;j<dp.length;j++){
-                if(i==0&&j==0){
-                    dp[j]=1;
-                }
-                else if(coins[i]<=j){
+        dp[0]=1; // there is one combination to make 0 amount
+        for(int i=0;i<coins.length;i++){ //picking coins one by one
+            for(int j=coins[i];j<dp.length;j++){ // looping over each amount 0 to amount
+            // if amount is greator then coin the only we check as amount needs to be bigger to allow coins to combine themselves to formulate it now we add the ways by adding ways to make amount of amount-coin eg. 7 - 5 we will be adding ways for 2 to it. 
                     dp[j]+=dp[j-coins[i]];
-                }
             }
         }
         return dp[amount];
