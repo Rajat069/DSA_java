c97fc7d5dabd632c6c4918cd014966b4dfbe876e
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-08-12 11:05:08 +0530

    Time: 2 ms (68.10%), Space: 43.7 MB (71.96%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -2,11 +2,11 @@ class Solution {
     public int calculateMinimumHP(int[][] dungeon) {
          int[][]dp = new int[dungeon.length][dungeon[0].length];
          int r=dungeon.length-1,c=dungeon[0].length-1;
-         dp[r][c]=dungeon[r][c]>0?1:Math.abs(dungeon[r][c])+1;
-         for(int i=r-1;i>=0;i--){
+         dp[r][c]=dungeon[r][c]>0?1:Math.abs(dungeon[r][c])+1; //if pos value then cap of 1 is used is negative then abs(val)+1
+         for(int i=r-1;i>=0;i--){  //for last collumn here collumn value is only dependent on down row elem-dung value if neg we use cap of 1
                dp[i][c]=Math.max((dp[i+1][c]-dungeon[i][c]),1);
          }
-         for(int i=c-1;i>=0;i--){
+         for(int i=c-1;i>=0;i--){ //last row
                dp[r][i]=Math.max((dp[r][i+1]-dungeon[r][i]),1);
          }
          for(int i=r-1;i>=0;i--){
