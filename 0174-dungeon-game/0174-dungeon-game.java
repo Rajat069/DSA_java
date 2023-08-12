bb8c5e454e6f69cd5bac72d0a4ad3bd34bbeb1bc
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-08-12 11:34:41 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (71.96%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -1,19 +1,20 @@
 class Solution {
     public int calculateMinimumHP(int[][] dungeon) {
-         int[][]dp = new int[dungeon.length][dungeon[0].length];
-         int r=dungeon.length-1,c=dungeon[0].length-1;
-         dp[r][c]=dungeon[r][c]>0?1:Math.abs(dungeon[r][c])+1; //if pos value then cap of 1 is used is negative then abs(val)+1
-         for(int i=r-1;i>=0;i--){  //for last collumn here collumn value is only dependent on down row elem-dung value if neg we use cap of 1
-               dp[i][c]=Math.max((dp[i+1][c]-dungeon[i][c]),1);
-         }
-         for(int i=c-1;i>=0;i--){ //last row
-               dp[r][i]=Math.max((dp[r][i+1]-dungeon[r][i]),1);
-         }
-         for(int i=r-1;i>=0;i--){
-             for(int j=c-1;j>=0;j--){
-               dp[i][j]=Math.max((Math.min(dp[i+1][j],dp[i][j+1])-dungeon[i][j]),1);
-             }
-         }
-        return dp[0][0];
+        return TopDown(dungeon,0,0,new int[dungeon.length][dungeon[0].length]);
+    }
+    private int TopDown(int[][]dungeon,int i,int j,int dp[][]){
+        if(i>=dungeon.length||j>=dungeon[0].length){
+            return Integer.MAX_VALUE;
+        }
+        if(dp[i][j]!=0){
+            return dp[i][j];
+        }
+        if(i==dungeon.length-1&&j==dungeon[0].length-1){
+            return Math.max(1,1-dungeon[i][j]);
+        }
+        int bot = TopDown(dungeon,i+1,j,dp);
+        int rig = TopDown(dungeon,i,j+1,dp);
+        dp[i][j] = Math.max(1,Math.min(bot,rig)-dungeon[i][j]);
+        return dp[i][j];
     }
 }
