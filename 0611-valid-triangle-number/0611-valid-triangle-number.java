d2e9636a40af3b84b8c56915db7b15345725e3b4
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-08-15 17:06:39 +0530

    Time: 25 ms (83.26%), Space: 42.8 MB (89.46%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -2,6 +2,23 @@ class Solution {
     public int triangleNumber(int[] nums) {
        int count=0;
        Arrays.sort(nums);
+       for(int i=nums.length-1;i>1;i--){
+           int l=0,r=i-1;
+           while(l<r){
+              if(nums[l]+nums[r]>nums[i]){
+                  count+=(r-l);
+                  r--;
+              }
+              else l++;
+           } 
+       }
+      return count;
+    }
+}
+/*
+ public int triangleNumber(int[] nums) {
+       int count=0;
+       Arrays.sort(nums);
        for(int i=0;i<nums.length-2;i++){
            for(int j=i+1;j<nums.length-1;j++){
                for(int q=j+1;q<nums.length;q++){
@@ -10,5 +27,5 @@ class Solution {
            }
        }
       return count;
-    }
-}
+    } TC:O(N^3)
+*/
