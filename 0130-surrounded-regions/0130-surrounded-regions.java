c01e682e1e7c10e9209cf388059ca0b1f6c4d056
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-08-19 11:24:48 +0530

    Time: 38 ms (5.05%), Space: 44.2 MB (36.29%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,88 +1,101 @@
 class Solution {
-    HashSet<Pair<Integer,Integer>> hs = new HashSet<>();
+    private HashSet<Pair<Integer, Integer>> hs = new HashSet<>();
+    private int[][] dir = {{1, 0}, {0, 1}, {0, -1}, {-1, 0}};
+
     public void solve(char[][] board) {
-        char[][]boardSol = new char[board.length][board[0].length];
+        int rows = board.length;
+        int cols = board[0].length;
+
         markBorderCells(board);
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(board[i][j]=='O'&&i!=0&&i!=board.length-1&&j!=0&&j!=board[0].length&&!hs.contains(new Pair<>(i,j))){
-                boolean r=false,l=false,u=false,d=false;
-                for(int q=j;q<board[0].length;q++){
-                     if(board[i][q]=='X'&&!hs.contains(new Pair<>(i,j))){
-                         r=true;
-                         break;
-                     }
+
+        char[][] boardSol = new char[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == 'O' && !hs.contains(new Pair<>(i, j))) {
+                    boolean r = false, l = false, u = false, d = false;
+
+                    for (int q = j; q < cols; q++) {
+                        if (board[i][q] == 'X' && !hs.contains(new Pair<>(i, q))) {
+                            r = true;
+                            break;
+                        }
+                    }
+
+                    for (int q = j; q >= 0; q--) {
+                        if (board[i][q] == 'X' && !hs.contains(new Pair<>(i, q))) {
+                            l = true;
+                            break;
+                        }
+                    }
+
+                    for (int q = i; q < rows; q++) {
+                        if (board[q][j] == 'X' && !hs.contains(new Pair<>(q, j))) {
+                            d = true;
+                            break;
+                        }
+                    }
+
+                    for (int q = i; q >= 0; q--) {
+                        if (board[q][j] == 'X' && !hs.contains(new Pair<>(q, j))) {
+                            u = true;
+                            break;
+                        }
+                    }
+
+                    if (r && l && u && d) {
+                        boardSol[i][j] = 'X';
+                    } else {
+                        boardSol[i][j] = board[i][j];
+                    }
+                } else {
+                    boardSol[i][j] = board[i][j];
                 }
-                for(int q=j;q>=0;q--){
-                      if(board[i][q]=='X'&&!hs.contains(new Pair<>(i,j))){
-                         l=true;
-                         break;
-                     }
-                }
-                for(int q=i;q<board.length;q++){
-                       if(board[q][j]=='X'&&!hs.contains(new Pair<>(i,j))){
-                          d=true;
-                          break; 
-                       }
-                }
-                for(int q=i;q>=0;q--){
-                        if(board[q][j]=='X'&&!hs.contains(new Pair<>(i,j))){
-                          u=true;
-                          break; 
-                       }
-                }
-                 if(r&&l&&u&&d){
-                    boardSol[i][j]='X';
-                 }   
-               }
-               if(boardSol[i][j]=='\u0000'){
-                   boardSol[i][j]=board[i][j];
-               } 
             }
         }
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                board[i][j]=boardSol[i][j];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                board[i][j] = boardSol[i][j];
             }
         }
     }
-    private void markBorderCells(char[][]board){
-        for(int i=0;i<board[0].length;i++){
-            if(board[0][i]=='O'){
-                boolean[][]vis = new boolean[board.length][board[0].length];
-                markhelper(board,0,i,vis);
+
+    private void markBorderCells(char[][] board) {
+        int rows = board.length;
+        int cols = board[0].length;
+
+        boolean[][] visited = new boolean[rows][cols];
+
+        for (int i = 0; i < cols; i++) {
+            if (board[0][i] == 'O') {
+                markhelper(board, 0, i, visited);
+            }
+            if (board[rows - 1][i] == 'O') {
+                markhelper(board, rows - 1, i, visited);
             }
         }
-        for(int i=0;i<board[0].length;i++){
-            if(board[board.length-1][i]=='O'){
-                boolean[][]vis = new boolean[board.length][board[0].length];
-                markhelper(board,board.length-1,i,vis);
+
+        for (int i = 0; i < rows; i++) {
+            if (board[i][0] == 'O') {
+                markhelper(board, i, 0, visited);
             }
-        }
-        for(int i=0;i<board.length;i++){
-            if(board[i][0]=='O'){
-                boolean[][]vis = new boolean[board.length][board[0].length];
-                markhelper(board,i,0,vis);
-            }
-        }
-        for(int i=0;i<board.length;i++){
-            if(board[i][board[0].length-1]=='O'){
-                boolean[][]vis = new boolean[board.length][board[0].length];
-                markhelper(board,i,board[0].length-1,vis);
+            if (board[i][cols - 1] == 'O') {
+                markhelper(board, i, cols - 1, visited);
             }
         }
     }
-    private int[][]dir= {{1,0},{0,1},{0,-1},{-1,0}};
-    private void markhelper(char[][]board,int i,int j,boolean[][]visited){
-        if(i<0||i>=board.length||j<0||j>=board[0].length||visited[i][j]||board[i][j]=='X'){
+
+    private void markhelper(char[][] board, int i, int j, boolean[][] visited) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] == 'X') {
             return;
         }
-        hs.add(new Pair<>(i,j));
-        visited[i][j]=true;
-        for(int d[]:dir){
-            int newr = i+d[0];
-            int newc = j+d[1];
-            markhelper(board,newr,newc,visited);
+        hs.add(new Pair<>(i, j));
+        visited[i][j] = true;
+        for (int[] d : dir) {
+            int newr = i + d[0];
+            int newc = j + d[1];
+            markhelper(board, newr, newc, visited);
         }
     }
 }
