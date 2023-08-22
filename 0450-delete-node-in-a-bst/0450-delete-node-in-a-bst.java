588ce1705ddd43a6c733995202921fa5765b9473
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-08-22 11:16:58 +0530

    Time: 0 ms (100.00%), Space: 45.4 MB (44.25%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -17,25 +17,25 @@ class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
         if(root==null)return null;
         if(root.val>key){
-            root.left=deleteNode(root.left,key);
+            root.left=deleteNode(root.left,key); //receive the returned value to actually delete nodes
         }
         else if(root.val<key){
             root.right=deleteNode(root.right,key);
         }
         else{
-            if(root.right!=null&&root.left!=null){
+            if(root.right!=null&&root.left!=null){ //condition one the target node has 2 children here find max out of left subtree then set the current root value to max and run recursive code to delete max (the max will be with no child as with any subtree the max is always a leaf node)
                 int max = maxVAL(root.left);
                 root.val=max;
                 root.left=deleteNode(root.left,max);
                 return root;
                 
-            }
+            }//if only right node is there by pass current node and attacj right to parent
             if(root.right!=null){
                 return root.right;
             }
             else if(root.left!=null){
                 return root.left;
-            }
+            } // if no child is present attach null
             else{
                 return null;
             }
