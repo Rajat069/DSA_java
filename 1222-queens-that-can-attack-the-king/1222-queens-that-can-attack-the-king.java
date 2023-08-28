128e09ae2053d869eaafba2084708d430ef0df5f
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-08-28 08:57:01 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (96.45%) - LeetHub

--- 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.java
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.java
@@ -1,14 +1,14 @@
 class Solution {
     public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
         List<List<Integer>>sol = new ArrayList<>();
-        HashSet<String>hs = new HashSet<>();
+        boolean[][] isQ = new boolean[8][8];
         for(int[]pos:queens){
-            hs.add(pos[0]+"-"+pos[1]);
+            isQ[pos[0]][pos[1]]=true;
         }
         int r=king[0],c=king[1];
         int count=0;
         for(int i=c;i<8;i++){
-            if(hs.contains(r+"-"+i)){
+            if(isQ[r][i]){
                 List<Integer>temp = new ArrayList<>();
                 temp.add(r);
                 temp.add(i);
@@ -17,7 +17,7 @@ class Solution {
             }
         }
         for(int i=c;i>=0;i--){
-            if(hs.contains(r+"-"+i)){
+            if(isQ[r][i]){
                List<Integer>temp = new ArrayList<>();
                 temp.add(r);
                 temp.add(i);
@@ -26,7 +26,7 @@ class Solution {
             }
         }
         for(int i=r;i<8;i++){
-            if(hs.contains(i+"-"+c)){
+            if(isQ[i][c]){
                List<Integer>temp = new ArrayList<>();
                 temp.add(i);
                 temp.add(c);
@@ -35,7 +35,7 @@ class Solution {
             }
         }
         for(int i=r;i>=0;i--){
-            if(hs.contains(i+"-"+c)){
+            if(isQ[i][c]){
                List<Integer>temp = new ArrayList<>();
                 temp.add(i);
                 temp.add(c);
@@ -43,8 +43,8 @@ class Solution {
                 break;
             }
         }
-        for(int i=r,j=c;i<8;i++,j++){
-            if(hs.contains(i+"-"+j)){
+        for(int i=r,j=c;i<8&&j<8;i++,j++){
+            if(isQ[i][j]){
                 List<Integer>temp = new ArrayList<>();
                 temp.add(i);
                 temp.add(j);
@@ -52,8 +52,8 @@ class Solution {
                 break;
             }
         }
-        for(int i=r,j=c;i>=0;i--,j--){
-            if(hs.contains(i+"-"+j)){
+        for(int i=r,j=c;i>=0&&j>=0;i--,j--){
+            if(isQ[i][j]){
                 List<Integer>temp = new ArrayList<>();
                 temp.add(i);
                 temp.add(j);
@@ -62,7 +62,7 @@ class Solution {
             }
         }
         for(int i=r,j=c;i<8&&j>=0;i++,j--){
-            if(hs.contains(i+"-"+j)){
+            if(isQ[i][j]){
                 List<Integer>temp = new ArrayList<>();
                 temp.add(i);
                 temp.add(j);
@@ -71,7 +71,7 @@ class Solution {
             }
         }
         for(int i=r,j=c;i>=0&&j<8;i--,j++){
-            if(hs.contains(i+"-"+j)){
+            if(isQ[i][j]){
                 List<Integer>temp = new ArrayList<>();
                 temp.add(i);
                 temp.add(j);
