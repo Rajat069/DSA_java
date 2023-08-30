8fc7ba43d646f68720d82ae12be4e36de65a4491
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-08-30 11:04:44 +0530

    Time: 4 ms (77.37%), Space: 58.4 MB (16.06%) - LeetHub

---
+++ 2541-minimum-operations-to-make-array-equal-ii/2541-minimum-operations-to-make-array-equal-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long minOperations(int[] nums1, int[] nums2, int k) {
+        if(k==0){
+            for(int i=0;i<nums1.length;i++){
+                if(nums1[i]!=nums2[i])return -1;
+            }
+            return 0;
+        }
+        long inc=0,dec=0;
+        for(int i=0;i<nums1.length;i++){
+            if(nums1[i]>nums2[i]){
+                if((nums1[i]-nums2[i])%k==0){
+                    dec+=(nums1[i]-nums2[i])/k;
+                }
+                else return -1;
+            }
+            else if(nums2[i]>nums1[i]){
+                 if((nums2[i]-nums1[i])%k==0){
+                    inc+=(nums2[i]-nums1[i])/k;
+                }
+                else return -1;
+            }
+        }
+        return Math.abs(inc-dec)>1?-1:inc>dec?inc:dec;
+    }
+}
