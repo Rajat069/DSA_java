bba2549129a9a4ed4d6d426af6ab792d1c72ec93
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-09-01 08:42:07 +0530

    Time: 2 ms (73.73%), Space: 46.4 MB (51.44%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -2,8 +2,13 @@ class Solution {
     public int[] countBits(int n) {
         int[] bit_count = new int[n+1];
         for(int i=0;i<=n;i++){
-            bit_count[i]=Integer.bitCount(i);
+            bit_count[i]=helper(i,bit_count);
         }
         return bit_count;
+    }
+    private int helper(int num,int[]ar){
+        if(num==0||num==1)return num;
+        if(ar[num]!=0)return ar[num];
+        return num%2==0?helper(num/2,ar):helper(num/2,ar)+1;
     }
 }
