e9194c6359ffb183feb78013f6cc6802c24538b5
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2023-10-19 09:00:12 +0530

    Time: 354 ms (6.77%), Space: 43.6 MB (16.04%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,23 +1,26 @@
+import java.util.HashSet;
+
 class Solution {
-    String sol="";
-    HashSet<String>hs = new HashSet<>();
+    String sol = "";
+    HashSet<String> hs = new HashSet<>();
+
     public String findDifferentBinaryString(String[] nums) {
-        int len=nums[0].length();
-        for(String s:nums){
+        int len = nums[0].length();
+        for (String s : nums) {
             hs.add(s);
         }
-        generateCombinations("",len);
+        generateCombinations(new StringBuilder(), len);
         return sol;
     }
-     private void generateCombinations(String current, int n) {
+
+    private void generateCombinations(StringBuilder current, int n) {
         if (n == 0) {
-             if(!hs.contains(current)){
-                 sol=current;
-                 return;
-             }
+            if (!hs.contains(current.toString())) {
+                sol = current.toString();
+            }
         } else {
-            generateCombinations(current + "0",n - 1);
-            generateCombinations(current + "1",n - 1);
+        generateCombinations(new StringBuilder(current).append("0"),n- 1);
+        generateCombinations(new StringBuilder(current).append("1"),n- 1);
         }
     }
 }
