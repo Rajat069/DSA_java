92e3cb3241fa44cfb3569e7425b5de552121bdad
Rajat Singh Samant <72992710+Rajat069@users.noreply.github.com>
2024-02-04 11:07:13 +0530

    Time: 113 ms (13.14%), Space: 45.7 MB (9.76%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -10,7 +10,7 @@ class Solution {
         int needTo=t.length(),r=0,l=0;
         while(true){
             boolean flag1=false,flag2=false;
-            while(r<s.length()&&matched<needTo){
+            while(r<s.length()&&matched<needTo){ //acquring the window where all the elements fits
                 char ch= s.charAt(r);
                  r++;
                 hm2.put(ch,hm2.getOrDefault(ch,0)+1);
@@ -19,25 +19,25 @@ class Solution {
                 }
                 flag1=true;
             }
-            while(l<r&&matched==needTo){
+            while(l<r&&matched==needTo){  //removing the character from left till we cannot anymore
                 String sub = s.substring(l,r);
                 char ch = s.charAt(l);
-                if(sol.length()==0||sub.length()<sol.length()){
+                if(sol.length()==0||sub.length()<sol.length()){  //updating each possible answer
                     sol=sub;
                 }
-                if(hm2.get(ch)==1){
+                if(hm2.get(ch)==1){  //decrementing the frequency
                     hm2.remove(ch);
                 }
                 else{
                     hm2.put(ch,hm2.get(ch)-1);
                 }
                 l++;
-                if(hm2.getOrDefault(ch,0)<hm1.getOrDefault(ch,0)){
+                if(hm2.getOrDefault(ch,0)<hm1.getOrDefault(ch,0)){ //reducing the matched character if it is replaced due to reducing window size
                     matched--;
                 }
                 flag2=true;
             }
-            if(!flag1&&!flag2){
+            if(!flag1&&!flag2){  //if both loops are completly exhausted
                 break;
             }
         }
